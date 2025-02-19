ce620e3fcb7400255f6da4ea83ba718f0ff276f4
Manny <107129718+mlgtcode@users.noreply.github.com>
2025-02-19 18:40:30 +0100

    Backward compatibility with rob

--- Raiffeisen Online Banking Benutzer.user.js
+++ Raiffeisen Online Banking Benutzer.user.js
@@ -2,16 +2,21 @@
 // @name          Raiffeisen Online Banking Benutzer
 // @description   Skript zum automatischen Ausfüllen des Benutzer bei der Anmeldung.
 // @match         https://auth.raiffeisen.it/ridp/*
+// @match         https://rob.raiffeisen.it/*
 // @version       1
 // @grant         none
 // @noframes      
 // @run-at        document-idle
 // ==/UserScript==
 
-function ROBUserFill(nutzer){
-     var Rform = document.forms[0];
-     var RElement = Rform.querySelector('input[name="loginUser"]');
-     var RNutzer = RElement.value = nutzer;
+function ROBUserFill(nutzer){  
+     if (window.location.href.startsWith("https://rob.raiffeisen.it")) {
+       document.getElementById('username').value = nutzer;
+     } else if (window.location.href.startsWith("https://auth.raiffeisen.it")) {
+       var Rform = document.forms[0];
+       var RElement = Rform.querySelector('input[name="loginUser"]');
+       var RNutzer = RElement.value = nutzer;
+     }
 }
 
 // Bitte den Nutzer hier eintragen
